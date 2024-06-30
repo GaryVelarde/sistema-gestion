9f08ce7844a8ca1cd9dbde7d4a57be8ee7778068
Gary Velarde Rios <garyvelarderips@gmail.com>
2024-06-29 23:08:51 -0500

    se prueba eventContent para diseño del card del evento

--- src/app/demo/components/pages/events/events.component.ts
+++ src/app/demo/components/pages/events/events.component.ts
@@ -8,111 +8,122 @@ import esLocale from '@fullcalendar/core/locales/es';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss'],
+    selector: 'app-root',
+    templateUrl: './events.component.html',
+    styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+    @ViewChild('calendar') calendarComponent: FullCalendarComponent;
 
-  // Define el arreglo de eventos
-  events: EventInput[] = [
-    {
-        title: 'Evento 1',
-        start: new Date(),
-        backgroundColor: '#FF5733', // Color de fondo del evento
-        borderColor: '#D32F2F',    // Color del borde del evento (opcional)
+    // Define el arreglo de eventos
+    events: EventInput[] = [
+        {
+            title: 'Evento 1',
+            start: new Date(),
+            backgroundColor: '#FF5733', // Color de fondo del evento
+            borderColor: '#D32F2F', // Color del borde del evento (opcional)
+            editable: true,
+            startResizable: true,
+            durationEditable: true,
+        },
+        {
+            title: 'Evento 2',
+            start: new Date(),
+            backgroundColor: '#337DFF',
+            borderColor: '#1565C0',
+            editable: true,
+            startResizable: true,
+            durationEditable: true,
+        },
+    ];
+
+    // Formulario reactivo
+    eventForm: FormGroup;
+    displayDialog: boolean = false;
+
+    calendarOptions: CalendarOptions = {
+        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        },
+        buttonText: {
+            today: 'Hoy',
+            month: 'Mes',
+            week: 'Semana',
+            day: 'Día',
+            list: 'Lista',
+        },
+        locale: esLocale,
+        dateClick: this.handleDateClick.bind(this),
+        eventClick: this.handleEventClick.bind(this),
         editable: true,
-        startResizable: true,
-        durationEditable: true,
-      },
-      {
-        title: 'Evento 2',
-        start: new Date(),
-        backgroundColor: '#337DFF',
-        borderColor: '#1565C0',
-        editable: true,
-        startResizable: true,
-        durationEditable: true,
-      },
-  ];
+        selectable: true,
+        eventResizableFromStart: true,
+        events: this.events,
+        eventDrop: this.handleEventDrop.bind(this),
+        eventResize: this.handleEventResize.bind(this),
+        //eventContent: this.customEventContent.bind(this),
+    };
 
-  // Formulario reactivo
-  eventForm: FormGroup;
-  displayDialog: boolean = false;
+    constructor(private fb: FormBuilder) {}
 
-  calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
-    },
-    buttonText: {
-      today: 'Hoy',
-      month: 'Mes',
-      week: 'Semana',
-      day: 'Día',
-      list: 'Lista'
-    },
-    locale: esLocale,
-    dateClick: this.handleDateClick.bind(this),
-    eventClick: this.handleEventClick.bind(this),
-    editable: true,
-    selectable: true,
-    eventResizableFromStart: true,
-    events: this.events,
-    eventDrop: this.handleEventDrop.bind(this),
-    eventResize: this.handleEventResize.bind(this),
-  };
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.eventForm = this.fb.group({
-      title: ['', Validators.required],
-      start: [null, Validators.required],
-      end: [null, Validators.required]
-    });
-  }
-
-  handleDateClick(arg) {
-    this.eventForm.reset();
-    this.eventForm.patchValue({
-      start: arg.date,
-      end: arg.date
-    });
-    this.displayDialog = true;
-  }
-
-  handleEventClick(arg) {
-    alert('Event clicked: ' + arg.event.title);
-  }
-
-  handleEventDrop(eventDropInfo) {
-    alert('Event dropped to ' + eventDropInfo.event.start);
-  }
-
-  handleEventResize(eventResizeInfo) {
-    alert('Event resized to ' + eventResizeInfo.event.end);
-  }
-
-  addEvent() {
-    if (this.eventForm.valid) {
-      const newEvent: EventInput = {
-        title: this.eventForm.value.title,
-        start: this.eventForm.value.start,
-        end: this.eventForm.value.end,
-        allDay: false,
-        editable: true,
-        startResizable: true,
-        durationEditable: true,
-      };
-      this.events.push(newEvent);
-      const calendarApi = this.calendarComponent.getApi();
-      calendarApi.addEvent(newEvent);
-      this.displayDialog = false;
+    ngOnInit() {
+        this.eventForm = this.fb.group({
+            title: ['', Validators.required],
+            start: [null, Validators.required],
+            end: [null, Validators.required],
+        });
     }
-  }
+
+    /*customEventContent(arg) {
+        // Puedes personalizar el contenido del evento aquí si es necesario
+        const element = document.createElement('div');
+        element.innerHTML = `
+      <b>${arg.timeText}</b><br>
+      ${arg.event.title}
+    `;
+        return { domNodes: [element] };
+    }*/
+
+    handleDateClick(arg) {
+        this.eventForm.reset();
+        this.eventForm.patchValue({
+            start: arg.date,
+            end: arg.date,
+        });
+        this.displayDialog = true;
+    }
+
+    handleEventClick(arg) {
+        alert('Event clicked: ' + arg.event.title);
+    }
+
+    handleEventDrop(eventDropInfo) {
+        alert('Event dropped to ' + eventDropInfo.event.start);
+    }
+
+    handleEventResize(eventResizeInfo) {
+        alert('Event resized to ' + eventResizeInfo.event.end);
+    }
+
+    addEvent() {
+        if (this.eventForm.valid) {
+            const newEvent: EventInput = {
+                title: this.eventForm.value.title,
+                start: this.eventForm.value.start,
+                end: this.eventForm.value.end,
+                allDay: false,
+                editable: true,
+                startResizable: true,
+                durationEditable: true,
+            };
+            this.events.push(newEvent);
+            const calendarApi = this.calendarComponent.getApi();
+            calendarApi.addEvent(newEvent);
+            this.displayDialog = false;
+        }
+    }
 }
